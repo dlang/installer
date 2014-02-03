808b00db47d9b64e0cce0415f3834e5c5c11260d
Martin Nowak <code@dawg.eu>
2014-02-03 03:02:27 +0100

    update symlinks
    - dmd.conf.5 is now in man5
    
    - added ddemangle and dustmite
    
    - sorted
    
    - shouldn't the binaries simply be installed to /usr/bin???

--- osx/post-install.sh
+++ osx/post-install.sh
@@ -6,13 +6,15 @@
 # License:: Public Domain
 #
 
+ln -sf /usr/share/dmd/bin/ddemangle /usr/bin/ddemangle
 ln -sf /usr/share/dmd/bin/dmd /usr/bin/dmd
-ln -sf /usr/share/dmd/bin/rdmd /usr/bin/rdmd
 ln -sf /usr/share/dmd/bin/dmd.conf /usr/bin/dmd.conf
 ln -sf /usr/share/dmd/bin/dumpobj /usr/bin/dumpobj
+ln -sf /usr/share/dmd/bin/dustmite /usr/bin/dustmite
 ln -sf /usr/share/dmd/bin/obj2asm /usr/bin/obj2asm
+ln -sf /usr/share/dmd/bin/rdmd /usr/bin/rdmd
 ln -sf /usr/share/dmd/man/man1/dmd.1 /usr/share/man/man1/dmd.1
-ln -sf /usr/share/dmd/man/man1/dmd.conf.5 /usr/share/man/man1/dmd.conf.5
 ln -sf /usr/share/dmd/man/man1/dumpobj.1 /usr/share/man/man1/dumpobj.1
 ln -sf /usr/share/dmd/man/man1/obj2asm.1 /usr/share/man/man1/obj2asm.1
 ln -sf /usr/share/dmd/man/man1/rdmd.1 /usr/share/man/man1/rdmd.1
+ln -sf /usr/share/dmd/man/man5/dmd.conf.5 /usr/share/man/man5/dmd.conf.5
