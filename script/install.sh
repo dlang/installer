c0763b8cf330f00b5c7e7d68d92c65cd1ee65c3c
Vladimir Panteleev <git@thecybershadow.net>
2017-07-18 02:40:15 +0000

    install.sh: Fix ShellCheck note SC2086
    "Double quote to prevent globbing and word splitting."
    
    https://github.com/koalaman/shellcheck/wiki/SC2086

--- script/install.sh
+++ script/install.sh
@@ -37,9 +37,9 @@ fatal() {
 
 # ------------------------------------------------------------------------------
 curl2() {
-    : ${CURL_USER_AGENT:="installer/install.sh $(command curl --version | head -n 1)"}
-    TIMEOUT_ARGS="--connect-timeout 5 --speed-time 30 --speed-limit 1024"
-    command curl $TIMEOUT_ARGS -fL -A "$CURL_USER_AGENT" "$@"
+    : "${CURL_USER_AGENT:="installer/install.sh $(command curl --version | head -n 1)"}"
+    TIMEOUT_ARGS=(--connect-timeout 5 --speed-time 30 --speed-limit 1024)
+    command curl "${TIMEOUT_ARGS[@]}" -fL -A "$CURL_USER_AGENT" "$@"
 }
 
 curl() {
@@ -105,9 +105,9 @@ esac
 
 check_tools() {
     while [[ $# -gt 0 ]]; do
-        if ! command -v $1 &>/dev/null &&
+        if ! command -v "$1" &>/dev/null &&
                 # detect OSX' liblzma support in libarchive
-                ! { [ $os-$1 == osx-xz ] && otool -L /usr/lib/libarchive.*.dylib | grep -qF liblzma; }; then
+                ! { [ "$os-$1" == osx-xz ] && otool -L /usr/lib/libarchive.*.dylib | grep -qF liblzma; }; then
             local msg="Required tool $1 not found, please install it."
             case $os-$1 in
                 osx-xz) msg="$msg http://macpkg.sourceforge.net";;
@@ -311,12 +311,12 @@ run_command() {
             if [ -d "$path/$2" ]; then
                 log "$2 already installed";
             else
-                install_compiler $2
+                install_compiler "$2"
             fi
             install_dub
-            write_env_vars $2
+            write_env_vars "$2"
 
-            if [ "$(basename $SHELL)" = fish ]; then
+            if [ "$(basename "$SHELL")" = fish ]; then
                 local suffix=.fish
             fi
             if [ "$verbosity" -eq 0 ]; then
@@ -333,7 +333,7 @@ Run \`deactivate\` later on to restore your environment."
             if [ -z "${2:-}" ]; then
                 fatal "Missing compiler argument for $1 command.";
             fi
-            uninstall_compiler $2
+            uninstall_compiler "$2"
             ;;
 
         list)
@@ -382,7 +382,7 @@ resolve_latest() {
             then
                 local url=http://nightlies.dlang.org/$compiler/LATEST
                 logV "Determing latest $compiler version ($url)."
-                compiler="dmd-$(fetch $url)"
+                compiler="dmd-$(fetch "$url")"
             fi
             ;;
         ldc)
@@ -491,7 +491,7 @@ find_gpg() {
 download_and_unpack() {
     local tmp name
     tmp=$(mkdtemp)
-    name="$(basename $1)"
+    name="$(basename "$1")"
 
     check_tools curl
     if [[ $name =~ \.tar\.xz$ ]]; then
@@ -502,7 +502,7 @@ download_and_unpack() {
 
     log "Downloading and unpacking $1"
     download "$1" "$tmp/$name"
-    if [ ! -z ${3:-} ]; then
+    if [ ! -z "${3:-}" ]; then
         verify "$3" "$tmp/$name"
     fi
     if [[ $name =~ \.tar\.xz$ ]]; then
@@ -517,8 +517,8 @@ download_and_unpack() {
 }
 
 verify() {
-    : ${GPG:=$(find_gpg)}
-    if [ $GPG = x ]; then
+    : "${GPG:=$(find_gpg)}"
+    if [ "$GPG" = x ]; then
         return
     fi
     if [ ! -f "$path/d-keyring.gpg" ]; then
@@ -668,14 +668,14 @@ install_dub() {
     rm -rf "$path/dub" "$path/dub-*"
     mv "$tmp" "$path/$dub"
     logV "Linking $path/dub -> $path/$dub"
-    ln -s $dub "$path/dub"
+    ln -s "$dub" "$path/dub"
 }
 
 # ------------------------------------------------------------------------------
 
 parse_args "$@"
-resolve_latest $compiler
-run_command ${command:-install} $compiler
+resolve_latest "$compiler"
+run_command ${command:-install} "$compiler"
 }
 
 _ "$@"
