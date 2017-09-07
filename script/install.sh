dlang/installer

commit 579306939fa5d8cca6b47c3c558bdf06d0d5daa0

install.sh: Fix ShellCheck note SC2015
"Note that A && B || C is not if-then-else. C may run when A is true."

https://github.com/koalaman/shellcheck/wiki/SC2015

script/install.sh
@@ -158,6 +158,11 @@ If no argument are provided, the latest DMD compiler will be installed.
 }
 
 command_help() {
+    if [ -z "$1" ]; then
+        usage
+        return
+    fi
+
     local _compiler='Compiler
 
   dmd|gdc|ldc           latest version of a compiler
@@ -277,14 +282,14 @@ parse_args() {
     done
 
     if [ -n "$_help" ]; then
-        [ -z "$command" ] && usage || command_help $command
+        command_help $command
         exit 0
     fi
     if [ -n "$_activate" ]; then
        if [ "${command:-install}" == "install" ]; then
            verbosity=0
        else
-           [ -z "$command" ] && usage || command_help $command
+           command_help $command
            exit 1
        fi
     fi
@@ -528,7 +533,8 @@ verify() {
 write_env_vars() {
     case $1 in
         dmd*)
-            [ $os = osx ] && local suffix= || local suffix=$model
+            local suffix
+            [ $os = osx ] || suffix=$model
             local binpath=$os/bin$suffix
             local libpath=$os/lib$suffix
             local dc=dmd

commit 0b1511dd027945a881bb995ae3c6f9922aa1e3fd

install.sh: Fix -h / --help
Fix "./install.sh: line 161: $1: unbound variable" when running
install.sh with -h or --help.

Regression introduced in commit
579306939fa5d8cca6b47c3c558bdf06d0d5daa0.

script/install.sh
@@ -158,7 +158,7 @@ If no argument are provided, the latest DMD compiler will be installed.
 }
 
 command_help() {
-    if [ -z "$1" ]; then
+    if [ -z "${1-}" ]; then
         usage
         return
     fi