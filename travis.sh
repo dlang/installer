1909f057d61e2525f6c2f68c6f0e2485dfe59358
Martin Nowak <code@dawg.eu>
2017-09-27 02:07:40 +0200

    basic test for help menus

--- travis.sh
+++ travis.sh
@@ -39,6 +39,18 @@ do
     fi
 done
 
+cmds=(install uninstall list update)
+for cmd in "${cmds[@]}"
+do
+    ./script/install.sh --help | grep -qF "$cmd"
+    ./script/install.sh -h | grep -qF "$cmd"
+    ./script/install.sh "$cmd" --help | tr -d '\n' | grep -q "Usage\s*install.sh $cmd"
+    ./script/install.sh "$cmd" -h | tr -d '\n' | grep -q "Usage\s*install.sh $cmd"
+done
+# remove is alias for uninstall
+./script/install.sh remove --help | tr -d '\n' | grep -q "Usage\s*install.sh uninstall"
+./script/install.sh remove -h | tr -d '\n' | grep -q "Usage\s*install.sh uninstall"
+
 # check whether all installations have been uninstalled successfully
 if bash script/install.sh list
 then
