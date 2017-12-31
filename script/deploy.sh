91a33225cf97d28aba71cd3438b692b41cfda21f
Martin Nowak <code@dawg.eu>
2017-12-31 20:03:34 +0100

    add missing acls to s3 upload

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,5 +7,5 @@ ROOT=$(dirname $0)
 ${GPG:-gpg2} --detach-sign install.sh
 scp "$ROOT/install.sh" "$ROOT/install.sh.sig" digitalmars.com:/usr/local/www/dlang.org/data/
 scp "$ROOT/install.sh" "$ROOT/install.sh.sig" nightlies.dlang.org:/var/www/builds/
-aws --profile ddo s3 cp "$ROOT/install.sh" s3://downloads.dlang.org/other/
-aws --profile ddo s3 cp "$ROOT/install.sh.sig" s3://downloads.dlang.org/other/
+aws --profile ddo s3 cp "$ROOT/install.sh" s3://downloads.dlang.org/other/ --acl public-read --cache-control max-age=604800
+aws --profile ddo s3 cp "$ROOT/install.sh.sig" s3://downloads.dlang.org/other/ --acl public-read --cache-control max-age=604800
