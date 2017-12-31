af230f5b98ea50dc51bd40785732d30e127807fa
Martin Nowak <code@dawg.eu>
2017-12-31 14:47:18 +0100

    verify sha256 checksums

--- windows/build_lld.bat
+++ windows/build_lld.bat
@@ -4,9 +4,15 @@ set ROOT=%CD%
 
 call "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
 
+echo d5b36c0005824f07ab093616bdff247f3da817cae2c51371e1d1473af717d895 lld.src.tar.xz> sha256sums
+echo 5fa7489fc0225b11821cab0362f5813a05f2bcf2533e8a4ea9c9c860168807b0 llvm.src.tar.xz>> sha256sums
+
 set LLVM_URL=http://releases.llvm.org/%LLVM_VER%
 appveyor DownloadFile %LLVM_URL%/lld-%LLVM_VER%.src.tar.xz  -FileName lld.src.tar.xz  || exit /B 1
 appveyor DownloadFile %LLVM_URL%/llvm-%LLVM_VER%.src.tar.xz -FileName llvm.src.tar.xz || exit /B 1
+
+:: e.g. from git installation
+sha256sum -c sha256sums || exit /B 1
 
 7z x llvm.src.tar.xz || exit /B 1
 7z x lld.src.tar.xz  || exit /B 1
