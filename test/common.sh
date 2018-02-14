0974b88346dd3c779af5922054900df091381c9b
Sebastian Wilzbach <seb@wilzba.ch>
2018-02-15 00:48:35 +0100

    Add 'get-path` to print the path of the installed compiler

--- test/common.sh
+++ test/common.sh
@@ -4,3 +4,19 @@ set -eu -o pipefail
 
 ROOT="$DIR/../"
 INSTALLER="$ROOT/script/install.sh"
+
+assert() {
+    actual="$1"
+    expected="$2"
+    if [ "$actual" != "$expected" ] ; then
+        echo "Actual: $actual"
+        echo "Expected: $expected"
+        exit 1
+    fi
+}
+
+err_report() {
+    echo "Error on line $1"
+}
+
+trap 'err_report $LINENO' ERR
