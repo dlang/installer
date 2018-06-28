8e51f72c3eadf62d5907cfe5f11be151fb086c0f
Sebastian Wilzbach <seb@wilzba.ch>
2018-06-28 11:48:13 +0200

    Minor install script deploy improvments

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,7 +4,9 @@ set -uexo pipefail
 
 ROOT=$(dirname $0)
 
-${GPG:-gpg2} --detach-sign install.sh
+rm -f $ROOT/install.sh.sig
+${GPG:-gpg2} --detach-sign $ROOT/install.sh
+
 scp "$ROOT/install.sh" "$ROOT/install.sh.sig" digitalmars.com:/usr/local/www/dlang.org/data/
 scp "$ROOT/install.sh" "$ROOT/install.sh.sig" nightlies.dlang.org:/var/www/builds/
 aws --profile ddo s3 cp "$ROOT/install.sh" s3://downloads.dlang.org/other/ --acl public-read --cache-control max-age=604800
