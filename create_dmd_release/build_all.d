d1d8b1b560b487bd8934e5c9ce731ed5229c0405
Sebastian Wilzbach <seb@wilzba.ch>
2018-07-11 16:44:01 +0200

    Update mingw to 5.0.2-1
    Fix Issue 19051 - Undefined functions Set/GetWindowLongPtr in mingw libs

--- create_dmd_release/build_all.d
+++ create_dmd_release/build_all.d
@@ -448,7 +448,7 @@ int main(string[] args)
     enum libC = "snn.lib";
     enum libCurl = "libcurl-7.52.1-WinSSL-zlib-x86-x64.zip";
     enum omflibs = "omflibs-winsdk-10.0.16299.15.zip";
-    enum mingwlibs = "mingw-libs-5.0.2.zip";
+    enum mingwlibs = "mingw-libs-5.0.2-1.zip";
     enum lld = "lld-link-5.0.1.zip";
 
     auto oldCompilers = platforms
