5e32f0b06ef56f54c5206b4c3063e4e9d8484070
Martin Kinkelin <noone@nowhere.com>
2018-10-22 01:04:35 +0200

    Auto-generate oldnames.c from special MinGW .def directives

--- windows/mingw/buildsdk.d
+++ windows/mingw/buildsdk.d
@@ -10,7 +10,7 @@
 // source files extracted from MinGW-w64:
 // https://sourceforge.net/projects/mingw-w64/files/mingw-w64/mingw-w64-release/mingw-w64-v6.0.0.tar.bz2
 //
-// assumes VC tools cl,link,lib and ml installed and found through path
+// assumes VC tools cl, lib and ml installed and found through path
 //  and configured to the appropriate architecture
 //
 
@@ -23,6 +23,10 @@ import std.stdio;
 import std.string;
 
 version = verbose;
+
+bool x64;
+string[string] weakSymbols; // weak name => real name
+
 
 void runShell(string cmd)
 {
@@ -38,7 +42,7 @@ void runShell(string cmd)
 }
 
 // Preprocesses a 'foo.def.in' file to 'foo.def'.
-void generateDef(bool x64, string inFile, string outFile)
+void generateDef(string inFile, string outFile)
 {
     const includeDir = buildPath(dirName(dirName(inFile)), "def-include");
     const archDefine = x64 ? "DEF_X64" : "DEF_I386";
@@ -54,12 +58,22 @@ void sanitizeDef(string defFile)
     {
         string l = line;
 
-        // The MinGW-w64 .def files specify renamings as 'exportedName == realName'.
-        // The MS format is 'exportedName=realName'.
-        l = l.replace(" == ", "=");
+        // The MinGW-w64 .def files specify weak external symbols as 'alias == realName'.
+        if (l.length > 1 && l[0] != ';')
+        {
+            const i = l.indexOf(" == ");
+            if (i > 0)
+            {
+                const weakName = l[0 .. i];
+                const realName = l[i+4 .. $];
+                weakSymbols[weakName] = realName;
+
+                l = ";" ~ l;
+            }
+        }
 
         // Don't export function 'atexit'; we have our own in msvc_atexit.c.
-        if (l == "atexit" /* 120 */ || l == "atexit DATA" /* < 120 */)
+        if (l == "atexit" /* msvcr120 */ || l == "atexit DATA" /* < 120 */)
             l = "";
 
         // Do export function '__chkstk' (ntdll.dll).
@@ -77,7 +91,7 @@ void sanitizeDef(string defFile)
         std.file.write(defFile, newLines.join("\n"));
 }
 
-void copyDefs(bool x64, string inDir, string outDir)
+void copyDefs(string inDir, string outDir)
 {
     mkdirRecurse(outDir);
 
@@ -90,7 +104,7 @@ void copyDefs(bool x64, string inDir, string outDir)
         if (lowerPath.endsWith(".def.in"))
         {
             outFile = buildPath(outDir, baseName(f.name)[0 .. $-7] ~ ".def");
-            generateDef(x64, path, outFile);
+            generateDef(path, outFile);
         }
         else if (lowerPath.endsWith(".def"))
         {
@@ -103,7 +117,7 @@ void copyDefs(bool x64, string inDir, string outDir)
     }
 }
 
-void def2implib(bool x64, string defFile)
+void def2implib(string defFile)
 {
     const libFile = setExtension(defFile, ".lib");
     const arch = x64 ? "X64" : "X86";
@@ -111,49 +125,44 @@ void def2implib(bool x64, string defFile)
     std.file.remove(setExtension(defFile, ".exp"));
 }
 
-void defs2implibs(bool x64, string dir)
+void defs2implibs(string dir)
 {
     foreach (f; std.file.dirEntries(dir, SpanMode.shallow))
     {
         const path = f.name;
         if (toLower(path).endsWith(".def"))
-            def2implib(x64, path);
+            def2implib(path);
     }
 }
 
-void buildMsvcrt(bool x64, string outDir)
+void buildMsvcrt(string outDir)
 {
     outDir ~= "/";
     const cl = "cl /c /Zl ";
     const lib = "lib /MACHINE:" ~ (x64 ? "X64" : "X86") ~ " ";
 
     // compile some additional objects to be merged into the msvcr*.lib files
-    runShell(cl ~ "/Fo" ~ outDir ~ "msvcrt_stub0.obj /D_APPTYPE=0 msvcrt_stub.c");
-    runShell(cl ~ "/Fo" ~ outDir ~ "msvcrt_stub1.obj /D_APPTYPE=1 msvcrt_stub.c");
-    runShell(cl ~ "/Fo" ~ outDir ~ "msvcrt_stub2.obj /D_APPTYPE=2 msvcrt_stub.c");
-    runShell(cl ~ "/Fo" ~ outDir ~ "msvcrt_data.obj msvcrt_data.c");
-    runShell(cl ~ "/Fo" ~ outDir ~ "msvcrt_atexit.obj msvcrt_atexit.c");
+    runShell(cl ~ `"/Fo` ~ outDir ~ `msvcrt_stub0.obj" /D_APPTYPE=0 msvcrt_stub.c`);
+    runShell(cl ~ `"/Fo` ~ outDir ~ `msvcrt_stub1.obj" /D_APPTYPE=1 msvcrt_stub.c`);
+    runShell(cl ~ `"/Fo` ~ outDir ~ `msvcrt_stub2.obj" /D_APPTYPE=2 msvcrt_stub.c`);
+    runShell(cl ~ `"/Fo` ~ outDir ~ `msvcrt_data.obj" msvcrt_data.c`);
+    runShell(cl ~ `"/Fo` ~ outDir ~ `msvcrt_atexit.obj" msvcrt_atexit.c`);
     auto objs = [ "msvcrt_stub0.obj", "msvcrt_stub1.obj", "msvcrt_stub2.obj", "msvcrt_data.obj", "msvcrt_atexit.obj" ];
     if (!x64)
     {
-        runShell("ml /c /Fo" ~ outDir ~ "msvcrt_abs.obj msvcrt_abs.asm");
+        runShell(`ml /c "/Fo` ~ outDir ~ `msvcrt_abs.obj" msvcrt_abs.asm`);
         objs ~= "msvcrt_abs.obj";
     }
 
     // merge into libs
-    const additionalMsvcrtObjs = objs.map!(a => outDir ~ a).join(" ");
+    const additionalMsvcrtObjs = objs.map!(a => `"` ~ outDir ~ a ~ `"`).join(" ");
     foreach (f; std.file.dirEntries(outDir[0 .. $-1], "msvcr*.lib", SpanMode.shallow))
-        runShell(lib ~ f.name ~ " " ~ additionalMsvcrtObjs);
-
-    // create oldnames.lib (expected by dmd)
-    runShell(cl ~ "/Fo" ~ outDir ~ "oldnames.obj oldnames.c");
-    runShell(lib ~ "/OUT:" ~ outDir ~ "oldnames.lib " ~ outDir ~ "oldnames.obj");
-    objs ~= "oldnames.obj";
+        runShell(lib ~ `"` ~ f.name ~ `" ` ~ additionalMsvcrtObjs);
 
     // create empty uuid.lib (expected by dmd, but UUIDs already in druntime)
     std.file.write(outDir ~ "empty.c", "");
-    runShell(cl ~ "/Fo" ~ outDir ~ "uuid.obj " ~ outDir ~ "empty.c");
-    runShell(lib ~ "/OUT:" ~ outDir ~ "uuid.lib " ~ outDir ~ "uuid.obj");
+    runShell(cl ~ `"/Fo` ~ outDir ~ `uuid.obj" "` ~ outDir ~ `empty.c"`);
+    runShell(lib ~ `"/OUT:` ~ outDir ~ `uuid.lib" "` ~ outDir ~ `uuid.obj"`);
     objs ~= "uuid.obj";
     std.file.remove(outDir ~ "empty.c");
 
@@ -161,20 +170,47 @@ void buildMsvcrt(bool x64, string outDir)
         std.file.remove(outDir ~ f);
 }
 
+void buildOldnames(string outDir)
+{
+    const cPrefix = x64 ? "" : "_";
+    const oldnames_c =
+        // access this __ref_oldnames symbol to drag in the generated linker directives (msvcrt_stub.c)
+        "int __ref_oldnames;\n" ~
+        weakSymbols.byKeyValue.map!(pair =>
+            `__pragma(comment(linker, "/alternatename:` ~ cPrefix ~ pair.key ~ `=` ~ cPrefix ~ pair.value ~ `"));`
+        ).join("\n");
+
+    version (verbose)
+        writeln("\nAuto-generated oldnames.c:\n----------\n", oldnames_c, "\n----------\n");
+
+    const src = buildPath(outDir, "oldnames.c");
+    std.file.write(src, oldnames_c);
+
+    const obj = setExtension(src, ".obj");
+    runShell(`cl /c /Zl "/Fo` ~ obj ~ `" "` ~ src ~ `"`);
+
+    const lib = setExtension(src, ".lib");
+    runShell(`lib /MACHINE:` ~ (x64 ? "X64" : "X86") ~ ` "/OUT:` ~ lib ~ `" "` ~ obj ~ `"`);
+
+    std.file.remove(src);
+    std.file.remove(obj);
+}
+
 void main(string[] args)
 {
-    const x64 = (args.length > 1 && args[1] == "x64");
+    x64 = (args.length > 1 && args[1] == "x64");
     const mingwDir = (args.length > 2 ? args[2] : "mingw-w64");
     string outDir = x64 ? "lib64" : "lib32";
     if (args.length > 3)
         outDir = args[3];
 
-    copyDefs(x64, buildPath(mingwDir, "mingw-w64-crt", "lib-common"), outDir);
-    copyDefs(x64, buildPath(mingwDir, "mingw-w64-crt", "lib" ~ (x64 ? "64" : "32")), outDir);
+    copyDefs(buildPath(mingwDir, "mingw-w64-crt", "lib-common"), outDir);
+    copyDefs(buildPath(mingwDir, "mingw-w64-crt", "lib" ~ (x64 ? "64" : "32")), outDir);
 
-    defs2implibs(x64, outDir);
+    defs2implibs(outDir);
 
-    buildMsvcrt(x64, outDir);
+    buildMsvcrt(outDir);
+    buildOldnames(outDir);
 
     //version (verbose) {} else
         foreach (f; std.file.dirEntries(outDir, "*.def", SpanMode.shallow))
