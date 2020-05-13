4cc120e94640a8228dcc8f5ccff795bc4ceaa21e
Bastiaan Veelo <Bastiaan@SARC.nl>
2020-05-13 16:15:58 +0200

    Tweaks.

--- create_dmd_release/create_dmd_release.d
+++ create_dmd_release/create_dmd_release.d
@@ -946,8 +946,8 @@ string[] gitVersionedFiles(string path)
     // auto gitOutput = runCapture("git ls-files").strip();
     auto toplevel = runCapture("git rev-parse --show-toplevel").strip;
     trace("toplevel = "~toplevel);
-    trace("getcwd = "~getcwd);
-    auto prefix = toplevel[getcwd.length + 1 .. $];
+    trace("getcwd() = "~getcwd());
+    auto prefix = getcwd()[toplevel.length + 1 .. $].translate(['\\', '/']);
     trace("prefix = "~prefix);
     auto gitOutput = runCapture("git ls-tree -r HEAD:"~prefix~" --name-only --full-tree").strip();
     // ^^^^
