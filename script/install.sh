2758e27387e7629c0719b12ac291aa613273e6d3
Andrei Alexandrescu <andrei@erdani.com>
2020-06-28 21:53:49 -0400

    Update script/install.sh
    Co-authored-by: Sebastian Wilzbach <seb@wilzba.ch>

--- script/install.sh
+++ script/install.sh
@@ -903,7 +903,7 @@ write_env_vars() {
     echo "    unset -f deactivate"
     echo "}"
     echo
-    echo "[[ -v _OLD_D_PATH ]] && deactivate"
+    echo "if [ -v _OLD_D_PATH ] ; then deactivate; fi"
     echo "_OLD_D_PATH=\"\${PATH:-}\""
 
     if [ -n "$libpath" ] ; then
