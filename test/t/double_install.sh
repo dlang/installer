87408f5f22e627597dbc01318a13ae71c6b1b2de
Geod24 <pro.mathias.lang@gmail.com>
2020-12-29 15:22:07 +0900

    Upgrade double_install test to work on newer OSX
    Versions prior to v2.087.1 do not work on Mac OSX 10.15,
    so upgrade the versions as they are inconsequential to the test.

--- test/t/double_install.sh
+++ test/t/double_install.sh
@@ -7,10 +7,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../
 
 priorDMD=$(dmd --version || echo "no dmd")
 
-. $(./script/install.sh install dmd-2.082.1 -a)
+. $(./script/install.sh install dmd-2.090.1 -a)
 test "$(dmd --version | grep -oE '[^ ]+$' | head -n1 | tr -d '\r')" = "v2.082.1"
 
-. $(./script/install.sh install dmd-2.083.1 -a)
+. $(./script/install.sh install dmd-2.091.1 -a)
 test "$(dmd --version | grep -oE '[^ ]+$' | head -n1 | tr -d '\r')" = "v2.083.1"
 
 deactivate
