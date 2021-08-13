8e4ccaa48c2749273319493c4799664b03b6db00
Iain Buclaw <ibuclaw@gdcproject.org>
2021-08-13 14:00:45 +0200

    .cirrus.yml: Replace the shutdown travis CI pipeline

--- test/t/dub.sh
+++ test/t/dub.sh
@@ -6,22 +6,22 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../
 . $DIR/common.sh
 
 compilers=(
-    dub-1.10.0
-    dub-1.11.0
-    dmd-2.080.0,dub-1.14.0
+    dub-1.21.0
+    dub-1.22.0
+    dmd-2.088.1,dub-1.23.0
 )
 
 versions=(
-    'DUB version 1.10.0, built on '
-    'DUB version 1.11.0, built on '
-    'DUB version 1.14.0, built on '
+    'DUB version 1.21.0, built on '
+    'DUB version 1.22.0, built on '
+    'DUB version 1.23.0, built on '
 )
 
 for idx in "${!compilers[@]}"
 do
     compiler="${compilers[$idx]}"
 
-    if [[ "${TRAVIS_OS_NAME:-}" == "windows" && "$compiler" == *dub-1.14.0 ]]; then
+    if [[ "${OS_NAME:-}" == "windows" && "$compiler" == *dub-1.23.0 ]]; then
         continue # https://github.com/dlang/dub/issues/1795
     fi
 
@@ -37,9 +37,9 @@ do
 done
 
 # test latest dub works
-"$INSTALLER" dmd-2.080.0,dub
+"$INSTALLER" dmd-2.088.1,dub
 . $("$INSTALLER" $compiler -a)
-if [ "${TRAVIS_OS_NAME:-}" != "windows" ]; then # https://github.com/dlang/dub/issues/1795
+if [ "${OS_NAME:-}" != "windows" ]; then # https://github.com/dlang/dub/issues/1795
     dub --version
 fi
 
